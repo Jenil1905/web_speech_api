73a8d3a088ca615947eb817a85a65672c9ac75ef
Jenil1905 <langaliyajenil@gmail.com>
2025-09-27 00:37:27 +0530

    stop the recognition on the click

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { useState } from 'react'
-import { FaMicrophone } from 'react-icons/fa6'
+import { FaMicrophone, FaMicrophoneSlash } from 'react-icons/fa6'
 
 import './App.css'
 
 function App() {
-   
+
   // console.log(window.SpeechRecognition|| window.webkitSpeechRecognition)   --Check if browser supports or not--
   const recognition = new SpeechRecognition()
 
 
   return (
-    <div>
+    <div className='flex flex-col'>
       <div className='bg-gray-900 text-white font-bold text-6xl p-5 text-center w-[50%] rounded-xl m-auto mt-10 hover:scale-105 duration-100 transition-all'>
-        <FaMicrophone className='text-red-600 p-3 inline'/>
-        <button className='cursor-pointer' onClick={()=>recognition.start()}>Start Recording</button>
+        <FaMicrophone className='text-red-600 p-3 inline' />
+        <button className='cursor-pointer' onClick={() => recognition.start()}>Start Recording</button>
       </div>
+        <div className='bg-gray-900 text-white font-bold text-6xl p-5 text-center w-[50%] rounded-xl m-auto mt-10 hover:scale-105 duration-100 transition-all'>
+        <FaMicrophoneSlash className='text-red-600 p-3 inline' />
+          <button className='cursor-pointer' onClick={()=>{recognition.stop()}}>Stop Recording</button>
+        </div>
     </div>
   )
 }
