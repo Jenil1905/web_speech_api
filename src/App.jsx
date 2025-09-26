0f3c101ec8d122a73c931d82eacc5a84987c4a3a
Jenil1905 <langaliyajenil@gmail.com>
2025-09-27 00:11:37 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { FaMicrophone } from 'react-icons/fa6'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='bg-gray-900 text-white font-bold text-6xl p-5 text-center w-[50%] rounded-xl m-auto mt-10 hover:scale-105 duration-100 transition-all'>
+        <FaMicrophone className='text-red-600 p-3 inline'/>
+        <button className='cursor-pointer'>Start Recording</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
