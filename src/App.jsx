9deb432d452778a42ad43dbc86c73c3506bd9689
Jenil1905 <langaliyajenil@gmail.com>
2025-09-27 00:31:34 +0530

    start the recognition on clicking the button

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { FaMicrophone } from 'react-icons/fa6'
 import './App.css'
 
 function App() {
+   
+  // console.log(window.SpeechRecognition|| window.webkitSpeechRecognition)   --Check if browser supports or not--
+  const recognition = new SpeechRecognition()
+
 
   return (
     <div>
       <div className='bg-gray-900 text-white font-bold text-6xl p-5 text-center w-[50%] rounded-xl m-auto mt-10 hover:scale-105 duration-100 transition-all'>
         <FaMicrophone className='text-red-600 p-3 inline'/>
-        <button className='cursor-pointer'>Start Recording</button>
+        <button className='cursor-pointer' onClick={()=>recognition.start()}>Start Recording</button>
       </div>
     </div>
   )
